74fa2593284def4ec0e720a68de4e326eb4740fc
YadavYashvant <yashvantyadav855@gmail.com>
2023-07-20 19:25:24 +0530

    ViewPager2 Implemented

--- app/src/main/java/com/example/viewpager_app/MainActivity.kt
+++ app/src/main/java/com/example/viewpager_app/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.viewpager_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import me.relex.circleindicator.CircleIndicator
 
 class MainActivity : AppCompatActivity() {
+
+    private var titlesList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        postToList()
+
+        val viewpager:ViewPager2 = findViewById(R.id.viewPager)
+        viewpager.adapter = ViewPagerAdapter(titlesList,imagesList)
+        viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+    private fun addToList(title: String, image:Int) {
+        titlesList.add(title)
+        imagesList.add(image)
+
+
+    }
+
+    private fun postToList(){
+        val imgarr = arrayOf(R.drawable.gwagon,R.drawable.cake,R.drawable.house1,R.drawable.spiritual,R.drawable.ishuai)
+        val titlearr = arrayOf("Gwagon","Cake","House","God","Ishu")
+        for (i in 0..4) {
+            addToList(titlearr[i],imgarr[i])
+        }
+
     }
 }
